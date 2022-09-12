e7b2a877cba51d42e27f424f9ac864680137eeb4
t0933095470 <thethao165@gmail.com>
2022-09-12 09:38:32 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Conditional Operator (? :)
+
+int num = 40;
+String result = (num % 2 == 0) ? "Even" : "Odd";
+Console.WriteLine(num + " is an " + result + " number.");
+
+
+//Null Coalescing Operator (??)
+
+string name = null;
+//set username = name, if name is not null.
+//set username = “user”, if name is null.
+string username = name ?? "user";
+Console.WriteLine(username);
+
+
